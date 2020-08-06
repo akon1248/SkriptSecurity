cfc868292356fa903f94b41737416f9f88f5795e
souhon <43487668+souhon@users.noreply.github.com>
2020-08-06 23:42:57 +0900

    Update SkriptSecurity.java

--- src/main/java/com/akon/skriptsecurity/SkriptSecurity.java
+++ src/main/java/com/akon/skriptsecurity/SkriptSecurity.java
@@ -27,52 +27,45 @@ public class SkriptSecurity extends SecurityManager {
 
 	private void checkSkript(Permission perm) {
 		if ((perm instanceof FilePermission || perm instanceof SocketPermission || perm instanceof URLPermission || (perm instanceof RuntimePermission && (perm.getName().equals("readFileDescriptor") || perm.getName().equals("writeFileDescriptor")))) && Bukkit.getPluginManager().isPluginEnabled("Skript")) {
-			for (StackTraceElement ste : new Throwable().getStackTrace()) {
-				String className = ste.getClassName();
-				if (className.startsWith("net.minecraft.server." + SkriptSecurityMain.getVersion())) {
-					try {
-						Class<?> clazz = Class.forName(className);
-						if (clazz.getProtectionDomain().getCodeSource().getLocation().equals(SERVER_JAR_LOCATION)) {
-							return;
-						}
-					} catch (ClassNotFoundException e) {
-						e.printStackTrace();
-					}
-				} else if (className.equals("ch.njol.skript.lang.TriggerItem") && ste.getMethodName().equals("walk")) {
-					StringBuilder message = new StringBuilder();
-					if (perm instanceof FilePermission) {
-						String actions = perm.getActions();
-						ArrayList<String> actionTexts = Lists.newArrayList();
-						if (actions.contains("delete")) {
-							actionTexts.add("削除");
-						}
-						if (actions.contains("execute")) {
-							actionTexts.add("実行");
-						}
-						if (actions.contains("read")) {
-							actionTexts.add("読み取り");
-						}
-						if (actions.contains("write")) {
-							actionTexts.add("書き込み");
-						}
-						if (actions.contains("readlink")) {
-							actionTexts.add("リンクの読み取り");
-						}
-						message.append("ファイルの").append(String.join("/", actionTexts.toArray(new String[0]))).append("はできません (").append(perm.getName()).append(")");
-					} else if (perm instanceof SocketPermission || perm instanceof URLPermission) {
-						message.append(perm.getName()).append("との通信はできません");
-					} else if (perm.getName().equals("readFileDescriptor")) {
-						message.append("ファイルの読み取りはできません");
-					} else if (perm.getName().equals("writeFileDescriptor")) {
-						message.append("ファイルの書き込みはできません");
-					}
-					String warningMessage = "SkriptSecurityによってSkriptがブロックされました: " + message.toString();
-					Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("skriptsecurity.admin")).forEach(player -> player.sendMessage(ChatColor.RED + warningMessage));
-					SkriptSecurityMain.getInstance().getLogger().warning(warningMessage);
-					throw new SecurityException(message.toString());
+			StackTraceElement stack = Thread.currentThread().getStackTrace()[4];
+			String callerClassName = stack.getClassName();
+			boolean isNMS = callerClassName.startsWith("net.minecraft.server." + SkriptSecurityMain.getVersion());
+			boolean isTriggerItem = callerClassName.equals("ch.njol.skript.lang.TriggerItem");
+			boolean isWalkMethod = stack.getMethodName().equals("walk");
+			if(!isNMS && isTriggerItem && isWalkMethod){
+			StringBuilder message = new StringBuilder();
+			if (perm instanceof FilePermission) {
+				String rawActions = perm.getActions();
+				ArrayList<String> actions = Lists.newArrayList();
+				if (rawActions.contains("delete")) {
+					actions.add("削除");
 				}
+				if (rawActions.contains("execute")) {
+					actions.add("実行");
+				}
+				if (rawActions.contains("read")) {
+					actions.add("読み取り");
+				}
+				if (rawActions.contains("write")) {
+					actions.add("書き込み");
+				}
+				if (rawActions.contains("readlink")) {
+					actions.add("リンクの読み取り");
+				}
+				String combinedActions = String.join("/", actions.toArray(new String[0]));
+				message.append(String.format("ファイルの%sはできません (%s)", combinedActions, perm.getName()));
+			} else if (perm instanceof SocketPermission || perm instanceof URLPermission) {
+				message.append(perm.getName()).append("との通信はできません");
+			} else if (perm.getName().equals("readFileDescriptor")) {
+				message.append("ファイルの読み取りはできません");
+			} else if (perm.getName().equals("writeFileDescriptor")) {
+				message.append("ファイルの書き込みはできません");
+			}
+			String warningMessage = "SkriptSecurityによってSkriptがブロックされました: " + message.toString();
+			Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("skriptsecurity.admin")).forEach(player -> player.sendMessage(ChatColor.RED + warningMessage));
+			SkriptSecurityMain.getInstance().getLogger().warning(warningMessage);
+			throw new SecurityException(message.toString());
 			}
 		}
 	}
-
 }
