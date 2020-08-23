518b1cd63ef2fff176f0cbc622c048647f94dcb7
akon1248 <akon8321@gmail.com>
2020-08-23 16:47:40 +0900

    1.1
    ・MundoSKによるパケット送受信の制限機能を追加
    ・ほかのプラグインの機能を利用してファイルにアクセスした場合許可することもできる機能を追加(WorldEditのschematicなど)

--- src/main/java/com/akon/skriptsecurity/SkriptSecurity.java
+++ src/main/java/com/akon/skriptsecurity/SkriptSecurity.java
@@ -1,45 +1,78 @@
 package com.akon.skriptsecurity;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilePermission;
+import java.io.IOException;
 import java.net.SocketPermission;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLPermission;
 import java.security.Permission;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 public class SkriptSecurity extends SecurityManager {
 
-	private static final URL SERVER_JAR_LOCATION = Bukkit.class.getProtectionDomain().getCodeSource().getLocation();
+	private final HashMultimap<String, URL> allowedPackages = HashMultimap.create();
+
+	public SkriptSecurity(Plugin... plugins) {
+		this.allowedPackages.put("net.minecraft.server." + SkriptSecurityMain.getVersion(), Bukkit.class.getProtectionDomain().getCodeSource().getLocation());
+		for (Plugin plugin: plugins) {
+			if (plugin != null) {
+				URL pluginLocation = plugin.getClass().getProtectionDomain().getCodeSource().getLocation();
+				try {
+					File file = new File(pluginLocation.toURI());
+					JarInputStream jar = new JarInputStream(new FileInputStream(file));
+					JarEntry entry;
+					JarEntry lastEntry = null;
+					while ((entry = jar.getNextJarEntry()) != null) {
+						if (entry.getName().endsWith(".class") && lastEntry != null && lastEntry.getName().endsWith("/")) {
+							String pkg = lastEntry.getName();
+							this.allowedPackages.put(pkg.substring(0, pkg.length() - 1).replace("/", "."), pluginLocation);
+						}
+						lastEntry = entry;
+					}
+				} catch (URISyntaxException | IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
 
 	@Override
 	public void checkPermission(Permission perm) {
-		checkSkript(perm);
+		this.checkSkript(perm);
 	}
 
 	@Override
 	public void checkPermission(Permission perm, Object context) {
-		checkSkript(perm);
+		this.checkSkript(perm);
 	}
 
 	private void checkSkript(Permission perm) {
 		if ((perm instanceof FilePermission || perm instanceof SocketPermission || perm instanceof URLPermission || (perm instanceof RuntimePermission && (perm.getName().equals("readFileDescriptor") || perm.getName().equals("writeFileDescriptor")))) && Bukkit.getPluginManager().isPluginEnabled("Skript")) {
-			for (StackTraceElement ste : new Throwable().getStackTrace()) {
+			Set<Map.Entry<String, Collection<URL>>> entries = this.allowedPackages.asMap().entrySet();
+			for (StackTraceElement ste: new Throwable().getStackTrace()) {
 				String className = ste.getClassName();
-				if (className.startsWith("net.minecraft.server." + SkriptSecurityMain.getVersion())) {
-					try {
-						Class<?> clazz = Class.forName(className);
-						if (clazz.getProtectionDomain().getCodeSource().getLocation().equals(SERVER_JAR_LOCATION)) {
-							return;
+				for (Map.Entry<String, Collection<URL>> entry: entries) {
+					if (className.matches(entry.getKey() + "\\.[^.]+")) {
+						try {
+							if (entry.getValue().contains(Class.forName(className).getProtectionDomain().getCodeSource().getLocation())) {
+								return;
+							}
+						} catch (ClassNotFoundException ex) {
+							ex.printStackTrace();
 						}
-					} catch (ClassNotFoundException e) {
-						e.printStackTrace();
 					}
+				}
+				if (className.matches("[^.]+")) {
 				} else if (className.equals("ch.njol.skript.lang.TriggerItem") && ste.getMethodName().equals("walk")) {
 					StringBuilder message = new StringBuilder();
 					if (perm instanceof FilePermission) {
@@ -68,9 +101,8 @@ public class SkriptSecurity extends SecurityManager {
 					} else if (perm.getName().equals("writeFileDescriptor")) {
 						message.append("ファイルの書き込みはできません");
 					}
-					String warningMessage = "SkriptSecurityによってSkriptがブロックされました: " + message;
-					Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("skriptsecurity.admin")).forEach(player -> player.sendMessage(ChatColor.RED + warningMessage));
-					SkriptSecurityMain.getInstance().getLogger().warning(warningMessage);
+					String warningMessage = String.format(SkriptSecurityMain.TEMPLATE, message);
+					SkriptSecurityMain.sendSecurityMessage(warningMessage);
 					throw new SecurityException(message.toString());
 				}
 			}
--- src/main/java/com/akon/skriptsecurity/SkriptSecurityMain.java
+++ src/main/java/com/akon/skriptsecurity/SkriptSecurityMain.java
@@ -1,34 +1,43 @@
 package com.akon.skriptsecurity;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkriptSecurityMain extends JavaPlugin {
 
+	@Getter
 	private static String version;
+	@Getter
 	private static SkriptSecurityMain instance;
+	public static final String TEMPLATE = "SkriptSecurityによってSkriptがブロックされました: %s";
 
 	@Override
 	public void onEnable() {
 		instance = this;
 		version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 		Bukkit.getScheduler().runTask(this, () -> {
-			System.setSecurityManager(new SkriptSecurity());
+			System.setSecurityManager(new SkriptSecurity(Bukkit.getPluginManager().getPlugin("ProtocolLib")));
 			this.getLogger().info("SkriptSecurity has been initialized.");
+			if (Bukkit.getPluginManager().isPluginEnabled("MundoSK") && Bukkit.getPluginManager().isPluginEnabled("ProtocolLib")) {
+				ProtocolLibrary.getProtocolManager().addPacketListener(new PacketSecurity(this, PacketType.Play.Client.WINDOW_CLICK));
+				this.getLogger().info("PacketSecurity for MundoSK has been initialized.");
+			}
 		});
 	}
 
 	@Override
 	public void onDisable() {
 		System.setSecurityManager(null);
+		ProtocolLibrary.getProtocolManager().removePacketListeners(this);
 	}
 
-	public static String getVersion() {
-		return version;
-	}
-
-	public static SkriptSecurityMain getInstance() {
-		return instance;
+	public static void sendSecurityMessage(String msg) {
+		Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("skriptsecurity.admin")).forEach(player -> player.sendMessage(ChatColor.RED + msg));
+		instance.getLogger().warning(msg);
 	}
 
 }
